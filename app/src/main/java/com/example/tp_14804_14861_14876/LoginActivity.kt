5dd7934aece81ebd7d67608d1e75e50a2439aafa
raulferrete09 <60934044+raulferrete09@users.noreply.github.com>
2020-11-20 23:03:18 +0000

    Version 1

---
+++ app/src/main/java/com/example/tp_14804_14861_14876/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tp_14804_14861_14876
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
