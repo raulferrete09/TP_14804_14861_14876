1b677d7bf9c665a37844c2cba4eb15c12fbc61aa
Diogo Pontes <diogomiguelpontes@gmail.com>
2020-11-22 21:58:24 +0000

    version 3- error gmail login

--- app/src/main/java/com/example/tp_14804_14861_14876/LoginActivity.kt
+++ app/src/main/java/com/example/tp_14804_14861_14876/LoginActivity.kt
@@ -6,24 +6,73 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
     var auth : FirebaseAuth? = null
-
+    var googleSignInClient : GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         var login_btn_signin = findViewById<Button>(R.id.login_btn_signin)
-
+        var login_btn_gmail = findViewById<Button>(R.id.login_btn_gmail)
         auth = FirebaseAuth.getInstance()
         login_btn_signin.setOnClickListener {
             signinAndSignup()
         }
+        login_btn_gmail.setOnClickListener {
+            googleLogin()
+        }
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("1082061836398-n5ovmh9vjckaqrm68r3omcl2pvbgvu9d.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
 
     }
+    fun googleLogin(){
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent,GOOGLE_LOGIN_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GOOGLE_LOGIN_CODE) {
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if(result.isSuccess){
+                var account = result.signInAccount
+                firebaseAuthWithGoogle(account)
+            }
+        }
+    }
+
+    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        var credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener {
+            task ->
+                if(task.isSuccessful){
+                    //Login
+                    println("aaaaacaaaaaaaaaaaaaaaa111111111111111111111111111111111aaaaaaaaaaaa")
+                    moveMainPage(task.result?.user)
+                }else{
+                    //Show the error message
+                    println("aaaabbbbbbbbbbbbbbbbbbbbbbbbbbb22222222222222222222bbbbbbbbbbbbbbbbbbb")
+                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
     fun signinAndSignup(){
         val login_et_email = findViewById<EditText>(R.id.login_et_email)
         val login_et_password = findViewById<EditText>(R.id.login_et_password)
