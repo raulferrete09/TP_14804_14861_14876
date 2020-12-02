6250cbcff03731f28462f5c1e2f7cf0bc25c836c
raulferrete09 <60934044+raulferrete09@users.noreply.github.com>
2020-12-02 02:44:26 +0000

    Sign Up

--- app/src/main/java/com/example/tp_14804_14861_14876/LoginActivity.kt
+++ app/src/main/java/com/example/tp_14804_14861_14876/LoginActivity.kt
@@ -1,14 +1,15 @@
 package com.example.tp_14804_14861_14876
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Base64
 import android.util.Log
-import android.util.Log.ERROR
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.facebook.AccessToken
 import com.facebook.CallbackManager
@@ -28,7 +29,6 @@ import com.google.firebase.auth.GoogleAuthProvider
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 import java.util.*
-import javax.security.auth.callback.Callback
 
 //import com.facebook.FacebookSdk;
 //import com.facebook.appevents.AppEventsLogger;
@@ -39,14 +39,23 @@ class LoginActivity : AppCompatActivity() {
     var googleSignInClient: GoogleSignInClient? = null
     var GOOGLE_LOGIN_CODE = 12502
     var callbackManager : CallbackManager? = null
+    lateinit var login_btn_signin:Button
+    lateinit var login_btn_gmail:Button
+    lateinit var login_btn_facebook:Button
+    lateinit var login_tv_signup: TextView
+    lateinit var login_et_email:EditText
+    lateinit var login_et_password:EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        var login_btn_signin = findViewById<Button>(R.id.login_btn_signin)
-        var login_btn_gmail = findViewById<Button>(R.id.login_btn_gmail)
-        var login_btn_facebook = findViewById<Button>(R.id.login_btn_facebook)
-
+        login_btn_signin = findViewById<Button>(R.id.login_btn_signin)
+        login_btn_gmail = findViewById<Button>(R.id.login_btn_gmail)
+        login_btn_facebook = findViewById<Button>(R.id.login_btn_facebook)
+        login_tv_signup = findViewById<TextView>(R.id.login_tv_signup)
+        login_et_email = findViewById<EditText>(R.id.login_et_email)
+        login_et_password = findViewById<EditText>(R.id.login_et_password)
 
         auth = FirebaseAuth.getInstance()
 
@@ -59,6 +68,9 @@ class LoginActivity : AppCompatActivity() {
         login_btn_facebook.setOnClickListener {
             facebookLogin()
         }
+        login_tv_signup.setOnClickListener {
+            signupaccount()
+        }
 
         var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken("1082061836398-n5ovmh9vjckaqrm68r3omcl2pvbgvu9d.apps.googleusercontent.com") //values.xml
@@ -69,6 +81,10 @@ class LoginActivity : AppCompatActivity() {
         callbackManager = CallbackManager.Factory.create()
     }
     //NFDt/SXshxmq1FhkMKa3tkEgyDI=
+
+    fun signupaccount(){
+        startActivity(Intent(this,SignUpActivity::class.java))
+    }
 
     fun printHashKey() {
         try {
@@ -160,8 +176,6 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun signinAndSignup(){
-        val login_et_email = findViewById<EditText>(R.id.login_et_email)
-        val login_et_password = findViewById<EditText>(R.id.login_et_password)
         auth?.createUserWithEmailAndPassword(login_et_email.text.toString(),login_et_password.text.toString())
             ?.addOnCompleteListener {
             task ->
@@ -178,8 +192,6 @@ class LoginActivity : AppCompatActivity() {
             }
     }
     fun signinEmail(){
-        val login_et_email = findViewById<EditText>(R.id.login_et_email)
-        val login_et_password = findViewById<EditText>(R.id.login_et_password)
         auth?.signInWithEmailAndPassword(login_et_email.text.toString(),login_et_password.text.toString())
             ?.addOnCompleteListener {
             task ->
