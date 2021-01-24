0d4cc752e53d247269e7f3f11afcdca42dbc08f2
Strong30Impact <joaopfaria30@outlook.com>
2021-01-24 04:09:09 +0000

    Error report pdf file created

--- app/src/main/java/com/example/tp_14804_14861_14876/Fragments/SubmissionsFragment.kt
+++ app/src/main/java/com/example/tp_14804_14861_14876/Fragments/SubmissionsFragment.kt
@@ -1,27 +1,38 @@
 package com.example.tp_14804_14861_14876.Fragments
 
-import android.Manifest
+import android.Manifest.permission
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import android.widget.AdapterView.OnItemSelectedListener
 import androidx.core.app.ActivityCompat
-import androidx.core.content.PermissionChecker.checkSelfPermission
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.tp_14804_14861_14876.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.itextpdf.text.*
+import com.itextpdf.text.FontFactory.TIMES_ROMAN
+import com.itextpdf.text.FontFactory.getFont
+import com.itextpdf.text.pdf.PdfName.TIMES_ROMAN
+import com.itextpdf.text.pdf.PdfWriter
+import com.itextpdf.text.pdf.parser.Line
 import com.squareup.picasso.Picasso
 import com.squareup.picasso.Transformation
 import jp.wasabeef.picasso.transformations.RoundedCornersTransformation
 import java.io.File
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -57,6 +68,8 @@ class SubmissionsFragment : Fragment(), View.OnClickListener, OnItemSelectedList
 
     var auth : FirebaseAuth? = null
     private val PERMISSION_CODE = 1000
+    lateinit var name: String
+    lateinit var email: String
 
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -141,7 +154,8 @@ class SubmissionsFragment : Fragment(), View.OnClickListener, OnItemSelectedList
         //Firebase info
         auth = FirebaseAuth.getInstance()
         val user: FirebaseUser? = auth?.currentUser
-        val name:String? = user?.displayName
+        name= user?.displayName.toString()
+        email = user?.email.toString()
         val id:String? = user?.uid
         var photo = user?.photoUrl
 
@@ -155,10 +169,19 @@ class SubmissionsFragment : Fragment(), View.OnClickListener, OnItemSelectedList
         Picasso.get().load(photo).transform(transformation).into(profile_iv_photo)
     }
 
+    @SuppressLint("WrongConstant")
     override fun onClick(v: View) {
         when (v.id) {
             R.id.submission_btn_firebase -> {
-                checkInformation()
+                if (checkPermissions()) {
+                    val folder = File(Environment.getExternalStorageDirectory().toString() + File.separator + "HVAC" + File.separator + "Reports")
+                    if (!folder.exists()) {
+                        folder.mkdirs()
+                        checkInformation()
+                    } else {
+                        checkInformation()
+                    }
+                }
             }
             R.id.submission_iv_addphoto -> {
                 if (checkPermissions()) {
@@ -167,39 +190,100 @@ class SubmissionsFragment : Fragment(), View.OnClickListener, OnItemSelectedList
             }
             R.id.submission_iv_addaudio -> {
                 if (checkPermissions()) {
-                selectFile()
+                    selectFile()
                 }
             }
         }
     }
 
     private fun selectFile() {
-
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                requireContext() as Activity,
-                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                PERMISSION_CODE
-            )
-        } else {
             var intent = Intent(Intent.ACTION_PICK)
             intent.setType("*/*")
             intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
             intent.action = Intent.ACTION_GET_CONTENT
             startActivityForResult(intent, 1000)
-        }
     }
 
     private fun checkInformation() {
         if(submission_spinner_machine.toString().isNotEmpty() && submission_spinner_intervation.toString().isNotEmpty() && report_ed_anomaly.text.isNotEmpty())  {
+            savePDF(email, name)
             mainFragment = MainFragment()
             transaction = fragmentManager?.beginTransaction()!!
             transaction.replace(R.id.drawable_frameLayout, mainFragment)
             transaction.commit()
+        }
+    }
+
+    private fun savePDF(email:String, name:String) {
+        //create object of Document class
+        val doc = com.itextpdf.text.Document()
+
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val data = timeStamp.substring(0, 4) + "/" + timeStamp.substring(4, 6) + "/" + timeStamp.substring(6, 8) + " at " + timeStamp.substring(9,11) + "h" + timeStamp.substring(11,13)
+        //val data = timeStamp.substring(0, 4) + "/" + timeStamp.substring(5, 6) + "/" + timeStamp.substring(7, 8) + " at " + timeStamp.substring(9,10) + ":" + timeStamp.substring(11,12)
+        val pathname = "Report" + "_" + timeStamp
+        val path = Environment.getExternalStorageDirectory().toString() + "/" + "HVAC/Reports/" + pathname + ".pdf"
+
+        try {
+            //create instance of PDFWriter class
+            PdfWriter.getInstance(doc, FileOutputStream(path))
+            //open the document for writing
+            doc.open()
+
+            //get text
+            val text_spinner_machine: String = submission_spinner_machine.selectedItem.toString()
+            val text_spinner_intervation = submission_spinner_intervation.selectedItem.toString()
+            val text_reportanomaly = report_ed_anomaly.text.toString()
+
+            //
+            val font_title: Font = Font(FontFactory.getFont(FontFactory.TIMES_BOLD,20.0f))
+            val font_text: Font = Font(FontFactory.getFont(FontFactory.TIMES_BOLD,14.0f))
+            val title = Chunk("Intervention report of $data", font_title)
+            val text_machine = Chunk("Machine",font_text )
+            val text_intervation = Chunk("Type of Intervation",font_text)
+            val text_report = Chunk("Report",font_text)
+            val text_photos = Chunk("Photos",font_text)
+            val text_audio = Chunk("Audios",font_text)
+            val text_name = Chunk("Name",font_text)
+            val text_email = Chunk("Email",font_text)
+
+            //create the pdf structure
+            doc.addAuthor("HVAC")
+            doc.add(Paragraph(Chunk(title)))
+            doc.add(Paragraph(" "))
+            doc.add(Paragraph(" "))
+            doc.add(Paragraph(" "))
+            doc.add(Paragraph(Chunk(text_name)))
+            doc.add(Paragraph(name))
+            doc.add(Paragraph(Chunk(text_email)))
+            doc.add(Paragraph(email))
+            doc.add(Paragraph(" "))
+            doc.add(Paragraph(" "))
+            doc.add(Paragraph(Chunk(text_machine)))
+            doc.add(Phrase(text_spinner_machine))
+            doc.add(Paragraph(Chunk(text_intervation)))
+            doc.add(Phrase(text_spinner_intervation))
+            doc.add(Paragraph(" "))
+            doc.add(Paragraph(" "))
+            doc.add(Paragraph(Chunk(text_report)))
+            doc.add(Paragraph(text_reportanomaly))
+            doc.add(Paragraph(" "))
+            doc.add(Paragraph(" "))
+            doc.add(Paragraph(Chunk(text_photos)))
+            //add uri photos
+            val anchor = Anchor("https://translate.google.com/?sl=pt&tl=en&text=Criar%20a%20estrutura%20do%20PDF&op=translate.html");
+            anchor.reference = "https://translate.google.com/?sl=pt&tl=en&text=Criar%20a%20estrutura%20do%20PDF&op=translate.html"
+            doc.add(anchor)
+            //doc.add(Paragraph("https://translate.google.com/?sl=pt&tl=en&text=Criar%20a%20estrutura%20do%20PDF&op=translate"))
+            doc.add(Paragraph(" "))
+            //add uri videos
+            doc.add(Paragraph(Chunk(text_audio)))
+            doc.close()
+
+            Toast.makeText(activity, "$pathname.pdf \nsaved to success", Toast.LENGTH_SHORT).show()
+
+        } catch (e: Exception) {
+            Toast.makeText(activity, e.message, Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -221,7 +305,7 @@ class SubmissionsFragment : Fragment(), View.OnClickListener, OnItemSelectedList
     private fun checkPermissions(): Boolean {
         if (ActivityCompat.checkSelfPermission(
                 requireContext(),
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
+                permission.WRITE_EXTERNAL_STORAGE
             ) == PackageManager.PERMISSION_GRANTED
         ) {
             //Permission Granted
@@ -231,14 +315,13 @@ class SubmissionsFragment : Fragment(), View.OnClickListener, OnItemSelectedList
             ActivityCompat.requestPermissions(
                 requireContext() as Activity,
                 arrayOf(
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    permission.WRITE_EXTERNAL_STORAGE
                 ),
                 111
             )
             return false
         }
     }
-
-
 }
+
 
